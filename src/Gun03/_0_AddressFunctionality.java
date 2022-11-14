81d5bb1292297213771bf025d94ff7b73257acc7
TechnoStudy <ismettechno@gmail.com>
2022-11-14 08:04:37 +0000

    Address functionality, stale element ve select wait inceleme

---
+++ src/Gun03/_0_AddressFunctionality.java
@@ -0,0 +1,112 @@
+package Gun03;
+
+import Utils.BaseDriver;
+import Utils.Tools;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+/*
+       Senaryo;
+       1- Siteyi açınız.
+       2- Adress Ekleyiniz.
+       3- En son adresi edit yaparak ad ve soyadı değiştirip kaydediniz.
+       4- En son adresi siliniz.
+ */
+public class _0_AddressFunctionality extends BaseDriver {
+
+     @Test
+     void addAddress()
+     {
+         WebElement addressBook=driver.findElement(By.linkText("Address Book"));
+         addressBook.click();
+
+         WebElement newAddress=driver.findElement(By.linkText("New Address"));
+         newAddress.click();
+
+         WebElement firsName=driver.findElement(By.id("input-firstname"));
+         firsName.sendKeys("Ahmet");
+
+         WebElement lastname=driver.findElement(By.id("input-lastname"));
+         lastname.sendKeys("Demir");
+
+         WebElement company=driver.findElement(By.id("input-company"));
+         company.sendKeys("Techno Study");
+
+         WebElement address1=driver.findElement(By.id("input-address-1"));
+         address1.sendKeys("21a London Road");
+
+         WebElement address2=driver.findElement(By.id("input-address-2"));
+         address2.sendKeys("RG2 5BJ");
+
+         WebElement city=driver.findElement(By.id("input-city"));
+         city.sendKeys("Reading");
+
+         WebElement postCode=driver.findElement(By.id("input-postcode"));
+         postCode.sendKeys("12345");
+
+         WebElement country=driver.findElement(By.id("input-country"));
+         Select countrySelect= new Select(country);
+         countrySelect.selectByIndex(10); // selectByVisibleText,selectValue implicitly wait ile çalıştı
+         // selectByVisibleText,selectValue ile sorunsuz çalışır ve beklemeye ihtiyacı olmaz
+
+         WebElement option=driver.findElement(By.cssSelector("select[id='input-zone']>option"));
+         wait.until(ExpectedConditions.stalenessOf(option));
+
+         WebElement State=driver.findElement(By.id("input-zone"));
+         Select StateSelect= new Select(State);
+         StateSelect.selectByIndex(6);  // selectByVisibleText,selectValue implicitly wait ile çalıştı
+         // selectByIndex implicitly wait  ile çalışmıyor.
+         // selectByVisibleText,selectValue ile sorunsuz çalışır ve beklemeye ihtiyacı olmaz
+
+         WebElement defadres=driver.findElement(By.cssSelector("label [name='default'][value='1']"));
+         defadres.click();
+
+         WebElement ctn=driver.findElement(By.cssSelector("input[type='submit']"));
+         ctn.click();
+
+         Tools.successMessageValidation();
+     }
+
+    @Test(dependsOnMethods = {"addAddress"})
+    void editAddress()
+    {
+        WebElement addressBook=driver.findElement(By.linkText("Address Book"));
+        addressBook.click();
+
+        List<WebElement> editAll=driver.findElements(By.linkText("Edit"));
+        WebElement sonEdit= editAll.get(editAll.size()-1); // son edit
+        sonEdit.click();
+
+        WebElement firsName=driver.findElement(By.id("input-firstname"));
+        firsName.clear();
+        firsName.sendKeys("Mehmet");
+
+        WebElement lastname=driver.findElement(By.id("input-lastname"));
+        lastname.clear();
+        lastname.sendKeys("Yılmaz");
+
+        WebElement ctn=driver.findElement(By.cssSelector("input[type='submit']"));
+        ctn.click();
+
+        Tools.successMessageValidation();
+    }
+
+    @Test(dependsOnMethods = {"editAddress"})
+    void deleteAddress()
+    {
+        WebElement addressBook=driver.findElement(By.linkText("Address Book"));
+        addressBook.click();
+
+        List<WebElement> deleteAll=driver.findElements(By.linkText("Delete"));
+        WebElement silinebilirAdres= deleteAll.get(deleteAll.size()-2); // son edit
+        silinebilirAdres.click();
+
+        Tools.successMessageValidation();
+    }
+
+}
