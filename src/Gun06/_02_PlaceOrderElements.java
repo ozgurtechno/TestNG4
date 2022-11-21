31087471c2154adc0e96502947ccd8f0701dea69
TechnoStudy <ismettechno@gmail.com>
2022-11-21 08:30:06 +0000

    Page Object Model : POM : WishList - Tools

--- src/Gun06/_02_PlaceOrderElements.java
+++ src/Gun06/_02_PlaceOrderElements.java
@@ -58,4 +58,5 @@ public class _02_PlaceOrderElements {
 
 
 
+
 }
--- src/Utils/Tools.java
+++ src/Utils/Tools.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
+import java.util.List;
+
 public class Tools{
 
     public static void Bekle(int saniye){
@@ -20,5 +22,28 @@ public class Tools{
         WebElement msgLabel= BaseDriver.driver.findElement(By.cssSelector("div[class='alert alert-success alert-dismissible'"));
         Assert.assertTrue(msgLabel.getText().toLowerCase().contains("success"));
     }
+
+    public static void listContainsString(List<WebElement> list, String aranacakKelime)
+    {
+        boolean bulundu = false;
+        for (WebElement e : list) {
+            if (e.getText().equalsIgnoreCase(aranacakKelime)) {
+                bulundu = true;
+                break;
+            }
+        }
+
+        if (!bulundu)
+            Assert.fail("Aranan text bulunamadı");
+    }
+
+    public static int RandomNumberGenerator(int max)  // size dan 4 geldiğinde 0,1,2,3 verir
+    {
+        return (int)(Math.random()* max);  // 0-max dahil
+
+        // Random rnd=new Random();
+        // return random.NextInt(max-1)+1;
+    }
+
 
 }
